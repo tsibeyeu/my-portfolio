6dd9a70e77cef9953fc4701700f95265ef06e114
tsion beyen <tsibeyeu@gmail.com>
2024-11-10 01:05:06 +0300

    added links

--- Desktop/all/React-projects/my-portfolio/src/pages/Home.jsx
+++ Desktop/all/React-projects/my-portfolio/src/pages/Home.jsx
@@ -1,11 +1,6 @@
 import style from "./Home.module.css";
 import { programmer } from "../component/images";
-import {
-  
-  FaGithub,
-  FaLinkedin,
-  FaTwitter,
-} from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import React, { useContext } from "react";
 import { themeContext } from "../component/Context";
 import About from "./About";
@@ -17,24 +12,35 @@ const Home = () => {
     <div>
       <main className={style.main}>
         <div className={style.left}>
-          
           <h1> I'm Tsion</h1>
-          <h2 style={{color:darkMode ? "#fff":"", }}>Full Stack Developer</h2>
-         
+          <h2 style={{ color: darkMode ? "#fff" : "" }}>
+            Full Stack Developer
+          </h2>
+
           <p>turning idea into interactive reality</p>
           <div className={style.link}>
-            <a style={{ color: "#345" }} href="">
+            <a style={{ color: "#345" }} href="https://github.com/tsibeyeu">
               {" "}
               <FaGithub />
             </a>
-            <a style={{ color: "#345" }} href="">
+            <a
+              style={{ color: "#345" }}
+              href="https://www.linkedin.com/in/tsion-beyene-10202b2b1"
+            >
               <FaLinkedin />
             </a>
-            <a style={{ color: "#345" }} href="">
+            <a style={{ color: "#345" }} href="https://x.com/TsionBeye">
               <FaTwitter />
             </a>
           </div>
-          <button className={style.btn}>download cv</button>
+          <a
+            className={style.btn}
+            href="Tsion Resume.pdf"
+            target="_blank"
+            download
+          >
+            download cv
+          </a>
         </div>
         <div className={style.right}>
           <img src={programmer} alt="hero-image" />
